961adefaadf0f14b8edd19f2725f4bae4e63bf06
Grant Jenks <grant.jenks@gmail.com>
2014-05-21 12:40:46 -0700

    Begin test creation: coverage and stress

---
+++ tests/test_stress.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import random, string
+from prioritydict import PriorityDict
+from nose.tools import raises
+from sys import hexversion
+
+def get_keysview(dic):
+    if hexversion < 0x03000000:
+        return dic.viewkeys()
+    else:
+        return dic.keys()
+
+def get_valuesview(dic):
+    if hexversion < 0x03000000:
+        return dic.viewvalues()
+    else:
+        return dic.values()
+
+def get_itemsview(dic):
+    if hexversion < 0x03000000:
+        return dic.viewitems()
+    else:
+        return dic.items()
+
+def test_stress():
+    pass
